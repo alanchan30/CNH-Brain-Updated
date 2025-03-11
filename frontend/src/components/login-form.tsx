8da37e9e8bf20f15fda69a066074136c3d80a1c9
Miguel Ventura <miguelventura1123@gmail.com>
2025-03-11 10:45:22 -0400

    responsive login for mobile

--- frontend/src/components/login-form.tsx
+++ frontend/src/components/login-form.tsx
@@ -17,13 +17,13 @@ export function LoginForm({
   return (
     <div className = "flex flex-col w-full h-full items-center justify-center">
     <img src="/childrens-white.png" alt="Logo" className="fixed top-0 left-0 p-4 w-48" />
-      <h1 className = "font-bold mb-10 text-6xl"> Welcome to Brain Visualizer</h1>
+      <h1 className = "font-bold mb-0 md:mb-10 text-6xl md:p-10 p-2 text-center"> Welcome to Brain Visualizer</h1>
 
-    <div className={cn("flex flex-col drop-shadow-2xl ring-6 ring-gray-400/20 rounded-2xl", className)} {...props}>
+    <div className={cn("flex flex-col drop-shadow-2xl ring-6 ring-gray-400/20 rounded-2xl m-10", className)} {...props}>
 
-      <Card className="w-[580px] p-12 border-none">
+      <Card className="md:w-[580px] md:p-12 p-2 pe-10 pb-10 border-none">
         <CardHeader>
-          <CardTitle className="text-2xl text-center font-bold">Log In using Children's National Hospital Credentials</CardTitle>
+          <CardTitle className="text-2xl text-center font-bold md:mt-0 mt-10">Log In using Children's National Hospital Credentials</CardTitle>
           {/* <CardDescription>
             Enter your email below to login to your account
           </CardDescription> */}
@@ -48,7 +48,7 @@ export function LoginForm({
                 <Input id="password" type="password" required placeholder="************" />
               </div>
               <div className="flex flex-col gap-3 items-center">
-                <Button type="submit" className="w-[300px] text-2xl">
+                <Button type="submit" className="md:w-[300px] w-[150px] text-2xl">
                   Login
                 </Button>
                 {/* <Button variant="outline" className="w-full">
