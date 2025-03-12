8b7129d2859ee27d986bb54c9a2a81dea6309798
Miguel Ventura <miguelventura1123@gmail.com>
2025-03-11 21:50:20 -0400

    init for redirect

---
+++ frontend/src/components/ui/header.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import logo from '@/assets/WhiteLogo.png'
+
+export default function Header() {
+  return (
+    <img src = {logo} alt="logo" width={240} height={156} />
+  )
+}
---
+++ frontend/src/pages/Redirect.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '@/components/ui/header'
+
+const Redirect = () => {
+  return (
+    <div>
+      <Header />
+      {/* TODO: rest of page */}
+    </div>
+  )
+}
+
+export default Redirect
