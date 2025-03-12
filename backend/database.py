820b6d5bee993285441b3ca48d092c766008c5fe
namanNagelia <nagelia.naman@gmail.com>
2025-03-11 22:19:00 -0400

    ORM Changes

--- backend/database.py
+++ backend/database.py
@@ -1,34 +1,63 @@
 from typing import List
 from typing import Optional
 from sqlalchemy import ForeignKey
-from sqlalchemy import String
+from sqlalchemy import create_engine, Column, Integer, String, DateTime
 from sqlalchemy import func
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import Mapped, sessionmaker
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import relationship
 from datetime import datetime
+from dotenv import load_dotenv
+import os
 
-class Base(DeclarativeBase):
-    pass
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+print(DATABASE_URL)
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable is not set")
+
+# Create SQLAlchemy engine
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
 
 class User(Base):
     __tablename__ = "user"
-    
+
     user_id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(30))
-    username: Mapped[str] = mapped_column(String(16))
-    password: Mapped[str] = mapped_column(String(16))
-    first_name: Mapped[str] = mapped_column(String(30))
-    last_name: Mapped[str] = mapped_column(String(30))
+    email: Mapped[str] = mapped_column(String(255))
+    username: Mapped[str] = mapped_column(String(255))
+    password: Mapped[str] = mapped_column(String(255))
+    first_name: Mapped[str] = mapped_column(String(255))
+    last_name: Mapped[str] = mapped_column(String(255))
     last_login: Mapped[datetime] = mapped_column(insert_default=func.now())
-    
+
+
 class FMRI_History(Base):
     __tablename__ = "fmri_history"
-    
+
     fmri_id: Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.user_id"))
     date: Mapped[datetime] = mapped_column(insert_default=func.now())
-    file_link: 
-    description:
-    title: 
+    file_link: Mapped[str] = mapped_column(String(255))
+    description: Mapped[str] = mapped_column(String(255))
+    title: Mapped[str] = mapped_column(String(255))
+
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+
+create_tables()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
